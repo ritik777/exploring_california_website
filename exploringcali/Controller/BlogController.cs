7ada62cec08300161c0ae387a6cece1ea6e8e629
ritik <kitri@DESKTOP-GNPDQT7>
2020-04-16 00:13:32 -0400

    added view

---
+++ exploringcali/Controller/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace MVC_BuildingBlocks.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "blog posts" };
+        }
+
+        [Route("blog/{year:min(1993)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            return new ContentResult { Content = string.Format("Year {0};Month {1}; Key {2};", year, month, key) };
+        }
+    }
+}
